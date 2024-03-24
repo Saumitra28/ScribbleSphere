e4484fc214e578d260a1766477b5f7dd8d5ab146
Saumitra <saumitraupadhyay28@gmail.com>
2024-03-25 00:39:31 +0530

    configured tailwind

--- ScribbleSphere/src/App.jsx
+++ ScribbleSphere/src/App.jsx
@@ -3,7 +3,11 @@ import conf from "./conf";
 
 function App() {
   console.log(conf.appwriteUrl);
-  return <>ScribbleSphere</>;
+  return(
+    <>
+    <h1 className="bg-gray-700 p-3 rounded-lg">ScribbleSphere</h1>
+    </>
+  )
 }
 
 export default App;
