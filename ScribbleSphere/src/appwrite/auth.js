33028ea551b8da4908678a165a1ff6fa3cd2d4de
Saumitra <saumitraupadhyay28@gmail.com>
2024-04-07 23:19:13 +0530

    resolved_some_issue

--- ScribbleSphere/src/appwrite/auth.js
+++ ScribbleSphere/src/appwrite/auth.js
@@ -1,56 +1,61 @@
-import conf from "./../conf";
 import { Client, Account, ID } from "appwrite";
+import conf from '../conf/conf';
 
-export class Authservice {
-  client = new Client();
-  constructor() {
-    this.client
-      .setEndpoint(conf.appwriteUrl)
-      .setProject(conf.appwriteProjectId);
-    this.account = new Account(this.client);
-  }
-  async createAccount({ email, password, name }) {
-    try {
-      const userAccount = await this.account.create(
-        ID.unique(),
-        email,
-        password,
-        name
-      );
-      if (userAccount) {
-        return this.login({ email, password });
-      } else {
-        return userAccount;
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
 
-  async login({ email, password }) {
-    try {
-      return await this.account.createEmailSession(email, password);
-    } catch (error) {
-      throw error;
-    }
-  }
-  async getCurrentUser() {
-    try {
-      return this.account.get();
-    } catch (error) {
-      console.log("Error: ", error);
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
     }
 
-    return null;
-  }
-  async logout() {
-    try {
-      await this.account.deleteSessions();
-    } catch (error) {
-      throw error;
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
     }
-  }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
 }
 
-const authService = new Authservice();
-export default authService;
+const authService = new AuthService();
+
+export default authService
