cbbef297bd40956a64e0f8f217e912ce19dddf8a
Saumitra <saumitraupadhyay28@gmail.com>
2024-04-07 23:01:15 +0530

    implemented_redux

--- ScribbleSphere/src/App.jsx
+++ ScribbleSphere/src/App.jsx
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-import conf from "./conf";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from './components/Header/Header'; 
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+ 
 
 function App() {
-  console.log(conf.appwriteUrl);
-  return(
-    <>
-    <h1 className="bg-gray-700 p-3 rounded-lg">ScribbleSphere</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>{/* TODO:  <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- ScribbleSphere/src/appwrite/config.js
+++ ScribbleSphere/src/appwrite/config.js
@@ -1,10 +1,11 @@
-import conf from "./../conf";
+import conf from "../conf/conf.js";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service {
   client = new Client();
   databases;
   bucket;
+
   constructor() {
     this.client
       .setEndpoint(conf.appwriteUrl)
@@ -12,6 +13,7 @@ export class Service {
     this.databases = new Databases(this.client);
     this.bucket = new Storage(this.client);
   }
+
   async createPost({ title, slug, content, featuredImage, status, userId }) {
     try {
       return await this.databases.createDocument(
@@ -27,47 +29,55 @@ export class Service {
         }
       );
     } catch (error) {
-      console.log("Error creating post:", error);
-      return null;
+      console.log("Appwrite serive :: createPost :: error", error);
     }
   }
-  async updatePost(slug, { title, content, featuredImage, status, userId }) {
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
     try {
       return await this.databases.updateDocument(
         conf.appwriteDatabaseId,
         conf.appwriteCollectionId,
-        slug
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
       );
     } catch (error) {
-      console.log("Error updating post:", error);
-      return null;
+      console.log("Appwrite serive :: updatePost :: error", error);
     }
   }
+
   async deletePost(slug) {
     try {
-      this.databases.deleteDocument(
+      await this.databases.deleteDocument(
         conf.appwriteDatabaseId,
-        conf.appwriteCollectionId
+        conf.appwriteCollectionId,
+        slug
       );
       return true;
     } catch (error) {
-      console.log("Error deleting post:", error);
+      console.log("Appwrite serive :: deletePost :: error", error);
       return false;
     }
   }
+
   async getPost(slug) {
     try {
-      return this.databases.getDocument(
+      return await this.databases.getDocument(
         conf.appwriteDatabaseId,
         conf.appwriteCollectionId,
         slug
       );
     } catch (error) {
-      console.log("Error getting post:", error);
-      return null;
+      console.log("Appwrite serive :: getPost :: error", error);
+      return false;
     }
   }
-  //unka status jo active h
+
   async getPosts(queries = [Query.equal("status", "active")]) {
     try {
       return await this.databases.listDocuments(
@@ -76,11 +86,13 @@ export class Service {
         queries
       );
     } catch (error) {
-      console.log("Error getting posts:", error);
-      return null;
+      console.log("Appwrite serive :: getPosts :: error", error);
+      return false;
     }
   }
-  //file upload
+
+  // file upload service
+
   async uploadFile(file) {
     try {
       return await this.bucket.createFile(
@@ -89,24 +101,25 @@ export class Service {
         file
       );
     } catch (error) {
-      console.log("Error uploading file:", error);
-      return null;
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      return false;
     }
   }
+
   async deleteFile(fileId) {
     try {
       await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
       return true;
     } catch (error) {
-      console.log("Error deleting file:", error);
+      console.log("Appwrite serive :: deleteFile :: error", error);
       return false;
     }
   }
+
   getFilePreview(fileId) {
     return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
   }
 }
 
 const service = new Service();
-
 export default service;
